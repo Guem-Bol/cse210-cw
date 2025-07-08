324b4567e24e7fc3f17864aa0f9da5250667a39f
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2025-07-08 13:48:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputuserName = DisplayPersonalMessage();
+     int inputNumber = DisplayNumber();
+     All(inputuserName ,inputNumber );
+
+        DisplayMessage();
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        DisplayPersonalMessage();
+
+        static string DisplayPersonalMessage()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+
+        return name;
+            
+            
+        }
+
+        DisplayNumber();
+
+        static int DisplayNumber()
+
+        {
+            int number = 0;
+            Console.Write("Please enter your favorite number: ");
+            number = int.Parse(Console.ReadLine());
+
+            int numb = number * number ;
+
+        return numb;
+
+
+        }
+
+        
+
+        static void All( string inputuserName, int  inputNumber)
+
+        {
+           
+         Console.WriteLine($"{inputuserName},The square of your number is {inputNumber}");
+
+            
+        }
+    }
+}
