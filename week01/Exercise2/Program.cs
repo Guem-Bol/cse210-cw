b5ea2b7f20af0dcd7aed09913aa70c29deb9f2a6
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2025-07-08 13:54:13 +0300

    Add files via upload

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grades? ");
+       string userinput = Console.ReadLine();
+       int number = int.Parse(userinput);
+
+       if (number >= 90)
+       {
+        Console.WriteLine("A");
+       }
+
+       else if (number >= 80)
+       {
+        Console.WriteLine("B");
+       }
+
+       else if (number >= 70)
+       {
+        Console.WriteLine("C");
+       }
+
+       else if (number >= 60)
+       {
+        Console.WriteLine("D");
+       }
+       else 
+       {
+        Console.WriteLine("F");
+       }
     }
 }
