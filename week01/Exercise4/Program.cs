acf2d7cffd7dcebdfc6ef5a75dc2929ecc76fc98
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2025-07-08 13:56:00 +0300

    Add files via upload

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+           List<int> numbers = new List<int>();    
+    int Number = 100;
+    
+    
+
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+    while (Number != 0)
+    
+    {
+        
+        Console.Write("Enter number: ");
+        Number = int.Parse(Console.ReadLine());
+
+         if (Number != 0)
+
+            {
+             numbers.Add(Number);
+            }
+      
+
+      
+        
+    }
+
+     int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int Max = numbers[0];
+
+        foreach (int number in numbers)
+
+        {
+
+            if (number > Max)
+            {
+                Max = number;
+                Console.WriteLine($"The maxinum number is: {Max}");
+               
+            }
+
+        
+        }
     }
 }
