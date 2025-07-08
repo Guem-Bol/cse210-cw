ebcadeec408769236e294fd93622d6a4b224661c
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2025-07-08 13:55:25 +0300

    Add files via upload

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your Guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+
+            }
+        }
     }
 }
