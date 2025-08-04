7254da37beae6cedb4e2bcbf98ec03c0309c7fea
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2025-08-04 15:25:37 +0300

    Add files via upload

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+
+        MathAssignment b = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(b.GetSummary());
+        Console.WriteLine(b.GetHomeworkList());
+
+        WritingAssignment c = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II ");
+        Console.WriteLine(c.GetSummary());
+        Console.WriteLine(c.GetWritingInformation());
+       
     }
 }
